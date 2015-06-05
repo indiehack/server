c285f16a4b25c670f0f05a7a0871a635a177c91f
mojzis <mojzisek@gmail.com>
2015-06-05 14:00:26 +0200

    added forever, removed mongoose

--- recipes/default.rb
+++ recipes/default.rb
@@ -26,14 +26,12 @@ include_recipe "nodejs::iojs"
 nodejs_npm 'typescript'
 
 # not necesarilly needed, is it ?
-nodejs_npm 'mongoose'
+nodejs_npm 'forever'
 nodejs_npm 'strongloop'
 nodejs_npm 'loopback-connector-mongodb'
 nodejs_npm 'mongo-express'
 nodejs_npm 'gulp'
 nodejs_npm 'bower'
-
-#nodejs_npm 'forever'
 
 # workaround - current Vagrant cant do it with ubuntu 15
 bash 'set-hostname' do 
